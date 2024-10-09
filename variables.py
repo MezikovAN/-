04302bfae3ceb5bdc2606971be0d1efbb99b8514
MezikovAN <malek31334@gmail.com>
2024-10-10 01:04:55 +0300

    переменные
    Практическое задание по работе в Pycharm - "Переменные".
    
    Напишите 4 переменных которые буду обозначать следующие данные:
    
    Количество выполненных ДЗ (запишите значение 12)
    
    Количество затраченных часов (запишите значение 1.5)
    
    Название курса (запишите значение 'Python')
    
    Время на одно задание (вычислить используя 1 и 2 переменные)

---
+++ variables.py
@@ -0,0 +1,9 @@
+number_of_compl_DZ = 12
+number_of_hours_spend = 1.5
+course_name = 'Python'
+time_one_task = number_of_hours_spend / number_of_compl_DZ
+print ('Курс:', course_name,
+       'всего задач:', number_of_compl_DZ,
+       'затрачено часов:', number_of_hours_spend,
+       'среднее время выполнения:', time_one_task,
+       'часа.')
